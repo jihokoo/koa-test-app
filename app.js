9c21a950257cc0a04779ae0ced744cdc46be0302
Ji Ho Koo <jihokoo@gmail.com>
2014-09-25 03:40:10 -0400

    add error handling

--- app.js
+++ app.js
@@ -12,9 +12,23 @@ app.use(function *(next){
 
 // response
 
+// app.use(function *(){
+//   this; // is the Context
+//   this.request; // is a koa Request
+//   this.response; // is a koa Response
+// });
+
 app.use(function *(){
   this.body = 'Hello World';
 });
+
+// for custom error handling, even without the code below koa will log
+// when it is not possible to respond to the client, koa will pass in the 'ctx' context object
+app.on('error', function(err, ctx){
+  log.error('server error', err, ctx);
+});
+
+
 
 app.listen(3000);
 // syntatic sugar for
